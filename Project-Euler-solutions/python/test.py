b21f306da3b6a23dc9e677e5aa4a48358bab9965
Layne Liu <layne.liu@outlook.com>
2022-09-27 12:44:42 +0200

    add another three projects

---
+++ Project-Euler-solutions/python/test.py
@@ -0,0 +1,18 @@
+# import eulerlib111
+
+# ans = eulerlib111.test()
+
+
+# a = 1
+# b =2 
+# a = False
+
+res = list()
+x = list()
+x.append(1.0)
+x.append("hello")
+for a in x:
+    for b in range(2):
+        res.append(b)
+    res.append(a)
+        
---
+++ test.py
@@ -0,0 +1,16 @@
+# a = 1
+# b = "hello"
+
+# def test():
+    # return a
+   
+# res = test()
+
+a = list()
+a.append(1)
+# b = list(a)
+
+class Test:
+    pass
+t = Test()
+t.x = 1
